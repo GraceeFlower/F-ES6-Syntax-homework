8a9e0873e24cdafc20f3ab19bdd83afa19b9e86e
GraceeFlower <1491644103@qq.com>
2020-10-16 13:20:47 +0800

    [Min] feat: practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = input => {
+    const {data, column} = input;
+    const output = [];
+    data.map(data => {
+        let member = {};
+        column.forEach((column, index) => member[column.name] = data[index]);
+        output.push(member);
+    });
+    return output;
+}
 export { parseData };
